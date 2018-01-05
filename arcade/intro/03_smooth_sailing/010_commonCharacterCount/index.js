18a5c0a17fa783b6c42bad062dae49cd00ded664
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-05 11:34:09 -0800

    refactored 010

--- arcade/intro/03_smooth_sailing/010_commonCharacterCount/index.js
+++ arcade/intro/03_smooth_sailing/010_commonCharacterCount/index.js
@@ -1,13 +1,10 @@
 const commonCharacterCount = (s1,s2) => {
   let total = 0;
-  const alpha = { bet: [] };
-  for (let i = 97; i < 123; i++) { alpha.bet.push(String.fromCodePoint(i)) }
-  alpha.bet.forEach(char => {
-    let lengths = [];
+  const alphabet = [];
+  for (let i = 97; i < 123; i++) { alphabet.push(String.fromCodePoint(i)) }
+  alphabet.forEach(char => {
     if (s1.indexOf(char) !== -1 && s2.indexOf(char) !== -1) {
-      [s1,s2].forEach(str => {
-        lengths.push(str.split('').filter(character => character === char).length)
-      });
+      let lengths = [s1,s2].map(str => str.split('').filter(character => character === char).length);
       total += lengths.sort((a,b) => a > b)[0];
     }
   })
