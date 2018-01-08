36748549632fe34df647c842d501d9578a0e5de2
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-08 14:08:39 -0800

    added 014, edited 013

--- arcade/intro/03_smooth_sailing/013_reverseParentheses/test/index.js
+++ arcade/intro/03_smooth_sailing/013_reverseParentheses/test/index.js
@@ -2,8 +2,13 @@ const expect = require('chai').expect;
 const reverseParentheses = require('..').reverseParentheses;
 
 describe('reverseParentheses()', () => {
-  it('should do what Im testing for', () => {
+  it('should reverse a single set of chars within a bracket sequence once', () => {
     expect(reverseParentheses('a(bc)de')).to.deep.equal('acbde');
   });
+
+  it('should reverse nested bracket seqs appropriately', () => {
+    expect(reverseParentheses('a()'))
+  });
+
 
 });
