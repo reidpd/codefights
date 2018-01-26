4a8b48bf2667ece5c5812c38fb038c6a90769002
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-26 15:33:50 -0800

    added 039

--- arcade/intro/09_dark_wilderness/039_knapsackLight/index.js
+++ arcade/intro/09_dark_wilderness/039_knapsackLight/index.js
@@ -1,5 +1,12 @@
-const rewrite_name = (/* replace with args */) => {
-  return true;
+const knapsackLight = (value1, weight1, value2, weight2, maxW) => {
+  if ((weight1 + weight2) <= maxW) { return value1 + value2 }
+
+  let treasures = [ { value: value1, weight: weight1 },
+                    { value: value2, weight: weight2 } ]
+                  .filter(item => item.weight <= maxW);
+
+  if (treasures.length < 2) { return !treasures.length ? 0 : treasures[0].value }
+  else { return treasures.sort((a,b) => a.value < b.value)[0].value; }
 };
 
-module.exports = { fns: [ /* fns */ ] }
+module.exports = { fns: [ knapsackLight ] };
