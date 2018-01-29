2e2f8992d3e1ca864a01884fc3b9c0978bcc47a5
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-28 16:47:57 -0800

    added 042

--- arcade/intro/09_dark_wilderness/042_bishopAndPawn/index.js
+++ arcade/intro/09_dark_wilderness/042_bishopAndPawn/index.js
@@ -1,5 +1,35 @@
-const rewrite_name = (/* replace with args */) => {
-  return true;
+// a -> h === W -> E, 1 -> 8 === S -> N
+
+const bishopAndPawn = (bishop, pawn) => {
+
+  let board = createBoard(), answer = false;
+  let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  let nums = ['8', '7', '6', '5', '4', '3', '2', '1'];
+  const bishop_coords = [nums.indexOf(bishop[1]), chars.indexOf(bishop[0])];
+  const increments = [-1, 1];
+
+  increments.forEach(y_adj => increments.forEach(x_adj => {
+    if (!answer) {
+      let y = bishop_coords[0] + y_adj, x = bishop_coords[1] + x_adj;
+      while (board[y] && board[y][x]) {
+        if (board[y][x] === pawn) { answer = true; break; }
+        else { y += y_adj; x += x_adj; }
+      }
+    }
+  }));
+
+  return answer;
 };
 
-module.exports = { fns: [ /* fns */ ] }
+const createBoard = () => {
+  let board = [];
+  let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], nums = [8,7, 6, 5,4,3,2,1];
+  nums.forEach(num => {
+    let row = [];
+    chars.forEach(char => row.push(char+num));
+    board.push(row);
+  })
+  return board;
+}
+
+module.exports = { fns: [ bishopAndPawn, createBoard ] }
--- arcade/intro/09_dark_wilderness/042_bishopAndPawn/test/index.js
+++ arcade/intro/09_dark_wilderness/042_bishopAndPawn/test/index.js
@@ -5,18 +5,81 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // bishopAndPawn()
       ins_and_outs: [
-        {
+        { // a -> h === W -> E, 1 -> 8 === S -> N
+          cxt: 'returns true for pawn that is two diagonals NE of bishop',
+          in: ['a1', 'c3'], // bishop, pawn
+          out: true,
+        }, {
           cxt: '',
-          in: [/* blank */],
-          out: ,
+          in: ['h1', 'h3'],
+          out: false,
+        }, {
+          cxt: 'simple test',
+          in: ['a5', 'c3'],
+          out: true,
+        }, {
+          cxt: 'simple test',
+          in: ['g1', 'f3'],
+          out: false,
+        }, {
+          cxt: 'simple test',
+          in: ['e7', 'd6'],
+          out: true,
+        }, {
+          cxt: 'simple test',
+          in: ['e7', 'a3'],
+          out: true,
+        }, {
+          cxt: 'simple test',
+          in: ['e3', 'a7'],
+          out: true,
+        }, {
+          cxt: 'simple test',
+          in: ['a1', 'h8'],
+          out: true,
+        }, {
+          cxt: 'simple test',
+          in: ['a1', 'h7'],
+          out: false,
+        }, {
+          cxt: 'simple test',
+          in: ['h1', 'a8'],
+          out: true,
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
           expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // createBoard()
+      ins_and_outs: [
+        {
+          cxt: 'should create an array of arrays of strings representing spaces on a chessboard',
+          in: [],
+          out: [
+            ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8'],
+            ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
+            ['a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6'],
+            ['a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5'],
+            ['a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4'],
+            ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3'],
+            ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
+            ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1']
+          ],
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          const result = param.fn();
+          param.out.forEach((row, i) => row.forEach((col, j) => {
+            expect(result[i][j]).to.deep.equal(col);
+          }));
         });
       }
     }/*, {
@@ -36,10 +99,11 @@ describe("testing ", () => {
       }
     }
     */
+
   ];
-  tests.forEach((test, idx) => test.fn = fns[idx]); // sets functions according to order set in index.js
+
   tests.forEach((test, testIdx) => {
-    test.fn = fns[idx]; // sets functions according to order set in index.js
+    test.fn = fns[testIdx]; // sets functions according to order set in index.js
     test.ins_and_outs.forEach(params => {
       params.fn = test.fn;
       test.run(params);
