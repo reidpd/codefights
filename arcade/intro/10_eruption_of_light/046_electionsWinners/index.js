f3946fd1846b6be5f840858f0ff456fbe9ea6085
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-29 12:45:23 -0800

    added 046

--- arcade/intro/10_eruption_of_light/046_electionsWinners/index.js
+++ arcade/intro/10_eruption_of_light/046_electionsWinners/index.js
@@ -1,5 +1,16 @@
-const funcName = () => {
-  return true;
+const electionsWinners = (/*array.integer*/ votes, /*integer*/ k ) => {
+  let winners = 0;
+  const mostVotes = votes[votes.indexOf(Math.max(...votes))];
+  const frontrunners = votes.filter(c => c === mostVotes);
+  if (frontrunners.length === 1) { winners++; }
+  else { winners += (k > 0) ? frontrunners.length : 0 }
+
+  const others = votes.filter(c => c < mostVotes);
+  others.forEach((candidate, candidateIdx) => {
+    const candidateCanWin = candidate + k > mostVotes;
+    if (candidateCanWin) winners++;
+  });
+  return winners;
 };
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ electionsWinners ] };
--- arcade/intro/10_eruption_of_light/046_electionsWinners/test/index.js
+++ arcade/intro/10_eruption_of_light/046_electionsWinners/test/index.js
@@ -5,12 +5,32 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // electionsWinners()
       ins_and_outs: [
         {
-          cxt: '',
-          in: [],
-          out: ,
+          cxt: 'returns number of potential candidates who can get a clear majority of votes over the rest',
+          in: [[2, 3, 5, 2], 3],
+          out: 2,
+        }, {
+          cxt: 'returns 0 if there is a tie && no one is left to cast new votes',
+          in: [[1, 3, 3, 1, 1], 0],
+          out: 0,
+        }, {
+          cxt: 'returns 1 if one candidate has a clear majority and no more votes are left',
+          in: [[5, 1, 3, 4, 1], 0],
+          out: 1,
+        }, {
+          cxt: 'returns the length of candidates if everyone is tied and only one vote is left',
+          in: [[1, 1, 1, 1], 1],
+          out: 4,
+        }, {
+          cxt: 'returns 0 if all candidates are tied and no votes are left',
+          in: [[1,1,1,1], 0],
+          out: 0,
+        }, {
+          cxt: 'returns 2 if two candidates are tied and no one else can top them with remaining votes',
+          in: [[3, 1, 1, 3, 1], 2],
+          out: 2,
         },
       ],
       run: param => {
