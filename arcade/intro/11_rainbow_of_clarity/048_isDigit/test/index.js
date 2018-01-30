a8ed036f8fcc76be4c38b9fe26301826532da47e
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-30 10:10:37 -0800

    slight mod to template/test, blank structure for chapter 11

--- arcade/intro/11_rainbow_of_clarity/048_isDigit/test/index.js
+++ arcade/intro/11_rainbow_of_clarity/048_isDigit/test/index.js
@@ -5,7 +5,7 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // isDigit()
       ins_and_outs: [
         {
           cxt: 'simple test',
