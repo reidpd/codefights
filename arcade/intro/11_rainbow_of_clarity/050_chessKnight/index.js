ad983bb19bf907f174ffc777531310d2bb4b774a
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-30 12:44:37 -0800

    added 050

--- arcade/intro/11_rainbow_of_clarity/050_chessKnight/index.js
+++ arcade/intro/11_rainbow_of_clarity/050_chessKnight/index.js
@@ -1,5 +1,38 @@
-const funcName = () => {
-  return true;
+const createBoard = require('../../09_dark_wilderness/042_bishopAndPawn').fns[1];
+
+const chessKnight = /*string*/ cell => { // str of 2 letters: coords for standard chess notation
+  const board = createBoard();
+  const coords = getCoords(cell, board);
+  const twoStepIncrements = [-2, 2];
+  const oneStepIncrements = [-1, 1];
+  const yX = ['y', 'x'];
+  let moves = 0;
+  twoStepIncrements.forEach((doubleStep, i) => {
+    oneStepIncrements.forEach((singleStep, j) => {
+      yX.forEach((coordName, coordIdx) => {
+        let doubleStepper = coordName, singleStepper = yX.slice().filter(c => c !== coordName)[0];
+        let stepCoords = coords => {
+          let clone = Object.assign({}, coords);
+          clone[doubleStepper] += doubleStep;
+          clone[singleStepper] += singleStep;
+          return clone;
+        };
+        let steppedCoords = stepCoords(coords);
+        if (board[steppedCoords.y] && board[steppedCoords.y][steppedCoords.x]) {
+          moves++;
+        }
+      })
+    })
+  })
+  return moves;
 };
 
-module.exports = { fns: [ funcName ] };
+const getCoords = (cell, board) => {
+  let result;
+  board.forEach((row, y) => row.forEach((col, x) => {
+    if (col === cell) { result = { y, x } };
+  }))
+  return result;
+}
+
+module.exports = { fns: [ chessKnight, getCoords ] };
--- arcade/intro/11_rainbow_of_clarity/050_chessKnight/test/index.js
+++ arcade/intro/11_rainbow_of_clarity/050_chessKnight/test/index.js
@@ -2,15 +2,56 @@ const expect = require('chai').expect;
 const fns = require('..').fns;
 console.log('fns === ', fns);
 
+const createBoard = require('../../../09_dark_wilderness/042_bishopAndPawn').fns[1];
+
 describe("testing ", () => {
   const tests = [
     {
       fn: '', // chessKnight()
       ins_and_outs: [
         {
-          cxt: 'simple test',
-          in: [],
-          out: true,
+          cxt: 'returns 2 if only two moves are possible',
+          in: ["a1"],
+          out: 2,
+        },{
+          cxt: 'returns 6 if only six moves are possible',
+          in: ["c2"],
+          out: 6,
+        },{
+          cxt: 'returns 8 if all moves are possible',
+          in: ["d4"],
+          out: 8,
+        },{
+          cxt: 'returns 6 if only 6 moves are possible',
+          in: ["g6"],
+          out: 6,
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // getCoords()
+      ins_and_outs: [
+        {
+          cxt: 'should return an object containing y & x indices for a given search item on a given board',
+          in: ["a1", createBoard()],
+          out: { y: 7, x: 0 },
+        },{
+          cxt: 'should return an object containing y & x indices for a given search item on a given board',
+          in: ["h8", createBoard()],
+          out: { y: 0, x: 7 },
+        },{
+          cxt: 'should return an object containing y & x indices for a given search item on a given board',
+          in: ["c5", createBoard()],
+          out: { y: 3, x: 2 },
+        },{
+          cxt: 'should return return undefined if the search item cannot be found',
+          in: ["j6", createBoard()],
+          out: undefined,
         },
       ],
       run: param => {
