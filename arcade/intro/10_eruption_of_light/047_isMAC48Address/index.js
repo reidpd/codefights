fbad81bd71677afadb951726f90f42e9241f4f52
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-01-30 10:01:10 -0800

    added 047

--- arcade/intro/10_eruption_of_light/047_isMAC48Address/index.js
+++ arcade/intro/10_eruption_of_light/047_isMAC48Address/index.js
@@ -1,5 +1,7 @@
-const funcName = () => {
-  return true;
+const isMAC48Address = (/*string*/ inputString) => { // 15 <= inputString.length <= 20
+  const split = inputString.split('-');
+  return split.length === 6 && split.every(pair =>
+    pair.length === 2 && pair.split('').every(char => !isNaN(parseInt(char, 16))));
 };
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ isMAC48Address ] };
--- arcade/intro/10_eruption_of_light/047_isMAC48Address/test/index.js
+++ arcade/intro/10_eruption_of_light/047_isMAC48Address/test/index.js
@@ -5,12 +5,48 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // isMAC48Address
       ins_and_outs: [
         {
-          cxt: '',
-          in: [],
-          out: ,
+          cxt: 'returns true for inputStrings that contain 6 groups of hexadecimal (0-9, A-F) digits separated by hyphens',
+          in: ["00-1B-63-84-45-E6"],
+          out: true,
+        },{
+          cxt: 'returns false if non-hexadecimal letters ("z") are used',
+          in: ["Z1-1B-63-84-45-E6"],
+          out: false,
+        },{
+          cxt: 'returns false for clearly non-conforming inputs (like human readable sentences)',
+          in: ["not a MAC-48 address"],
+          out: false,
+        },{
+          cxt: 'Accepts F as the only and last character',
+          in: ["FF-FF-FF-FF-FF-FF"],
+          out: true,
+        },{
+          cxt: 'Accepts 0 as the only and first character',
+          in: ["00-00-00-00-00-00"],
+          out: true,
+        },{
+          cxt: 'returns false if only one character does not conform to standard hexadecimal notation (the first)',
+          in: ["G0-00-00-00-00-00"],
+          out: false,
+        },{
+          cxt: 'returns false if extraneous hyphens are inserted',
+          in: ["02-03-04-05-06-07-"],
+          out: false,
+        },{
+          cxt: 'returns true for inputStrings that contain 6 groups of hexadecimal (0-9, A-F) digits separated by hyphens',
+          in: ["12-34-56-78-9A-BC"],
+          out: true,
+        },{
+          cxt: 'returns true for inputStrings that contain 6 groups of hexadecimal (0-9, A-F) digits separated by hyphens',
+          in: ["FF-FF-AB-CD-EA-BC"],
+          out: true,
+        },{
+          cxt: 'returns false if only one character does not conform to standard hexadecimal notation (the last)',
+          in: ["12-34-56-78-9A-BG"],
+          out: false,
         },
       ],
       run: param => {
