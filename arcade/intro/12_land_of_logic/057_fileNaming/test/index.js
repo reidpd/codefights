47f46a0d3668faeae61d4ac683c48350132c8439
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-02-03 10:31:01 -0800

    057 test mod, added 058

--- arcade/intro/12_land_of_logic/057_fileNaming/test/index.js
+++ arcade/intro/12_land_of_logic/057_fileNaming/test/index.js
@@ -12,11 +12,11 @@ describe("testing ", () => {
           in: ["doc", "doc", "image", "doc(1)", "doc"],
           out: ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"],
         },{
-          cxt: 'simple test',
+          cxt: 'when an iterated name replicates a previous unique name, the algorithm accounts for it with a new number attached',
           in: ["a(1)", "a(6)", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"],
           out: ["a(1)", "a(6)", "a", "a(2)", "a(3)", "a(4)", "a(5)", "a(7)", "a(8)", "a(9)", "a(10)", "a(11)"],
         },{
-          cxt: 'simple test',
+          cxt: 'iterated items do not duplicate previous uniques',
           in: ["dd", "dd(1)", "dd(2)", "dd", "dd(1)", "dd(1)(2)", "dd(1)(1)", "dd", "dd(1)"],
           out: ["dd", "dd(1)", "dd(2)", "dd(3)", "dd(1)(1)", "dd(1)(2)", "dd(1)(1)(1)", "dd(4)", "dd(1)(3)"],
         },
--- arcade/intro/12_land_of_logic/058_messageFromBinaryCode/index.js
+++ arcade/intro/12_land_of_logic/058_messageFromBinaryCode/index.js
@@ -1,5 +1,9 @@
-const messageFromBinaryCode = () => {
-  return true;
+const messageFromBinaryCode = code => {
+  let strands = [], start = 0, end = 1;
+  while (strands.length !== code.length / 8) {
+    strands.push(code.split('').slice((8*start++), (8*end++)).join(''));
+  }
+  return strands.map(str => String.fromCodePoint(parseInt(str, 2))).join('');
 };
 
 module.exports = { fns: [ messageFromBinaryCode ] };
