4da936981e5b77df02430439ac846fca4f3e17e7
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-02-04 20:58:01 -0800

    added interview-practice arrays 03

--- interview_practice/data_structures/arrays/03_rotateImage/js/index.js
+++ interview_practice/data_structures/arrays/03_rotateImage/js/index.js
@@ -1,5 +1,6 @@
-const rotateImage = () => {
-  return true;
+const rotateImage = a => {
+  const transposition = a.map((col, i) => a.map(row => row[i]));
+  return transposition.map(row => row.reverse());
 };
 
 module.exports = { fns: [ rotateImage ] };
