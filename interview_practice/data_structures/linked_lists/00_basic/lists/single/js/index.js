fc4e43deed511eb771c75c0665a3ced98c2f24e6
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-02-13 15:17:19 -0800

    edited lists/single, previous commit also added file structure for lists/double

--- interview_practice/data_structures/linked_lists/00_basic/lists/single/js/index.js
+++ interview_practice/data_structures/linked_lists/00_basic/lists/single/js/index.js
@@ -125,26 +125,5 @@ class List {
     return this;
   }
 }
-//
-// const createSingleElElFromArray = (arr) => {
-//   let newNode = node.createSingle(arr.shift());
-//   console.log(newNode);
-//   while (arr.length > 0) {
-//     newNode = traverseDownAndUpdate(newNode, 'next', null, createSingle(arr.shift()));
-//   }
-//   return newNode;
-// };
-//
-// const traverseDownAndUpdate = (node, searchProp, searchVal, updateVal) => {
-//   let newNode = node;
-//   if (searchVal) {
-//     while (!node[searchProp] || node[searchProp] !== searchVal) { console.log('!searchVal', newNode.next);newNode = newNode.next; }
-//   } else {
-//     while (!node[searchProp] || node[searchProp] !== searchVal) { console.log('searchVal');newNode = newNode.next; }
-//   }
-//   const nextVal = node.readProp(newNode, 'next') !== null ? newNode.next: null;
-//   node.updateReadProp(newNode, 'next', node.createSingle(updateVal, nextVal))
-//   return node;
-// }
 
 module.exports = { List, node };
