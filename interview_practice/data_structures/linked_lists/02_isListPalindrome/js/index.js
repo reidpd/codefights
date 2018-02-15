d1f3b38ce81071c9b816e9aebe2a70a1f6b37738
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-02-15 15:55:16 -0800

    added 02_isListPalindrome()

--- interview_practice/data_structures/linked_lists/02_isListPalindrome/js/index.js
+++ interview_practice/data_structures/linked_lists/02_isListPalindrome/js/index.js
@@ -1,5 +1,15 @@
-const isListPalindrome = () => {
-  return true;
-};
+// Definition for singly-linked list:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+
+function isListPalindrome(l) {
+  const list = [];
+  let node = l.head; // l
+  while (node) { list.push(node.value); node = node.next; }
+  let reversal = list.slice(0).reverse();
+  return reversal.every((item, idx) => list[idx] === item);
+}
 
 module.exports = { fns: [ isListPalindrome ] };
--- interview_practice/data_structures/linked_lists/02_isListPalindrome/js/test/index.js
+++ interview_practice/data_structures/linked_lists/02_isListPalindrome/js/test/index.js
@@ -2,15 +2,57 @@ const expect = require('chai').expect;
 const fns = require('..').fns;
 console.log('fns === ', fns);
 
+const List = require('../../../00_basic/lists/single/js/').List;
+
 describe("testing ", () => {
   const tests = [
     {
       fn: '', // isListPalindrome()
       ins_and_outs: [
         {
-          cxt: 'simple test',
-          in: [],
+          cxt: 'returns true if the list is identical in original and reversed orders',
+          in: [new List('value').fill([0,1,0])],
           out: true,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1,2,2,3])],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1, 1000000000, -1000000000, -1000000000, 1000000000, 1])],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1,2,3,3,2])],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1, 2, 3, 1, 2, 3])],
+          out: false,
+        },{
+          cxt: 'return true for empty lists',
+          in: [new List('value')],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([3,5,3,5])],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1,5,2,4])],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([10])],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([0,0])],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [new List('value').fill([1,3,2,2,2])],
+          out: false,
         },
       ],
       run: param => {
