e06818bd85eb1a14d48897580718617e7cd182ed
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-02-15 15:21:37 -0800

    submitted answer for linked_lists/01_removeKFromList, edited Node tests

--- interview_practice/data_structures/linked_lists/01_removeKFromList/js/index.js
+++ interview_practice/data_structures/linked_lists/01_removeKFromList/js/index.js
@@ -5,21 +5,18 @@
 // }
 //
 
+// This codefights challenge asks in the explanation for a linked list, but the tests are asking for arrays.
+
 const List = require('../../00_basic/lists/single/js/').List;
 
 const removeKFromList = (l, k) => {
-  const returnList = new List('value');
+  const returnList = new List('value'); // []
   let node = l;
   while (node) {
     if (node.value !== k) { returnList.push(node.value) }
     node = node.next;
   }
-  return returnList.head; // still not quite right
+  return returnList.head; // returnList
 };
-
-const listNode = x => {
-  this.value = x;
-  this.next = null;
-}
 
 module.exports = { fns: [ removeKFromList ] };
