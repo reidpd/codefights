b4e1d1621722f1bf45cd29d2a17374ca99bf3148
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-03-05 18:28:59 -0800

    added 018

--- arcade/the_core/03_corner_of_0s_and_1s/018_arrayPacking/js/index.js
+++ arcade/the_core/03_corner_of_0s_and_1s/018_arrayPacking/js/index.js
@@ -1,5 +1,3 @@
-const arrayPacking = () => {
-  return true;
-};
+const arrayPacking = a => a.reduce((value, byte, idx) => value + byte * Math.pow(256, idx));
 
 module.exports = { fns: [ arrayPacking ] };
--- arcade/the_core/03_corner_of_0s_and_1s/018_arrayPacking/js/test/index.js
+++ arcade/the_core/03_corner_of_0s_and_1s/018_arrayPacking/js/test/index.js
@@ -5,18 +5,34 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // arrayPacking()
+      fn: '', // arrayPacking(array)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [24,85,0],
+          out: 21784,
+        },{
+          cxt: 'simple test',
+          in: [23,45,39],
+          out: 2567447,
+        },{
+          cxt: 'simple test',
+          in: [1,2,4,8],
+          out: 134480385,
+        },{
+          cxt: 'simple test',
+          in: [5],
+          out: 5,
+        },{
+          cxt: 'simple test',
+          in: [187,99,42,43],
+          out: 724198331,
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
