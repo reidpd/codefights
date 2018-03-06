15a00571d5eb0a6bd139a3dc351301bac35d550b
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-03-05 18:13:08 -0800

    added arcade/the_core/03/017

--- arcade/the_core/03_corner_of_0s_and_1s/017_killKthBit/js/index.js
+++ arcade/the_core/03_corner_of_0s_and_1s/017_killKthBit/js/index.js
@@ -1,6 +1,31 @@
+/*
+
+Codefights solution:
+function killKthBit(n, k) {
+  return  n & ~(1 << k - 1);
+}
+
+*/
+
 const killKthBit = (n, k) => {
-  console.log(parseInt(n, 2));
-  return true;
+  if (n === 0) { return n }
+  let baseTwo = convertToBase2(n), idx = baseTwo.length - k;
+  let killMap = baseTwo.split('').map((x,i) => i === idx ? '0' : x).join('');
+  let bit = baseTwo[idx];
+  return bit === '0' ? n : convertToBase10(killMap);
 };
 
-module.exports = { fns: [ killKthBit ] };
+const convertToBase2 = (n) => {
+  let bits = [];
+  while (n !== 0) {
+    let remainder = n % 2;
+    bits.unshift(remainder);
+    n -= remainder;
+    n /= 2;
+  }
+  return bits.join('');
+}
+
+const convertToBase10 = str => str.split('').reverse().map((x,idx) => parseInt(x,10) * Math.pow(2,idx)).reduce((acc,val) => acc + val);
+
+module.exports = { fns: [ killKthBit, convertToBase2, convertToBase10 ] };
--- arcade/the_core/03_corner_of_0s_and_1s/017_killKthBit/js/test/index.js
+++ arcade/the_core/03_corner_of_0s_and_1s/017_killKthBit/js/test/index.js
@@ -8,46 +8,88 @@ describe("testing ", () => {
       fn: '', // killKthBit(n,k)
       ins_and_outs: [
         {
-          cxt: 'simple test',
+          cxt: '37 to base 2 needs to be decrement, so the new number 33 should be returned after conversion',
           in: [37,3],
           out: 33,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit is already 0, so return n',
           in: [37,4],
           out: 37,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit is already 0, so return n',
           in: [37,2],
           out: 37,
         },{
-          cxt: 'simple test',
+          cxt: 'input of n === 0 should return 0',
           in: [0,4],
           out: 0,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit equals 1, so kill the bit then return the new number after conversion',
           in: [2147483647,16],
           out: 2147450879,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit equals 1, so kill the bit then return the new number after conversion',
           in: [374823748,13],
           out: 374819652,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit equals 1, so kill the bit then return the new number after conversion',
           in: [2734827,4],
           out: 2734819,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit is already 0, so return n',
           in: [1084197039,15],
           out: 1084197039,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit equals 1, so kill the bit then return the new number after conversion',
           in: [1160825071,3],
           out: 1160825067,
         },{
-          cxt: 'simple test',
+          cxt: 'the xth-from-right bit is already 0, so return n',
           in: [2039063284,4],
           out: 2039063284,
         },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // convertToBase2
+      ins_and_outs: [
+        {
+          cxt: 'simple test',
+          in: [37],
+          out: "100101",
+        },{
+          cxt: 'simple test',
+          in: [33],
+          out: "100001",
+        }/*,{
+          cxt: 'simple test',
+          in: [],
+          out: "",
+        },*/
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // convertToBase10
+      ins_and_outs: [
+        {
+          cxt: 'simple test',
+          in: ["100101"],
+          out: 37,
+        },{
+          cxt: 'simple test',
+          in: ["100001"],
+          out: 33,
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
