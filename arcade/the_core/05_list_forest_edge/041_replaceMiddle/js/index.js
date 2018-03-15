fe44b4fd3e67ad7bdc1ff21a90bbad8c27261de3
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-03-15 14:13:10 -0700

    finished 05/041

--- arcade/the_core/05_list_forest_edge/041_replaceMiddle/js/index.js
+++ arcade/the_core/05_list_forest_edge/041_replaceMiddle/js/index.js
@@ -1,5 +1,15 @@
-const replaceMiddle = () => {
-  return true;
+const replaceMiddle = arr => {
+  if (arr.length % 2 === 1) { return arr }
+
+  let l = [], r = [];
+  for (let i = 0; i < arr.length/2; i++) {
+    if (i === (arr.length/2 - 1)) {
+      l.push(arr[i] + arr[i+1]);
+      return l.concat(r);
+    }
+    l.push(arr[i]);
+    r.unshift(arr[arr.length-1-i]);
+  }
 };
 
 module.exports = { fns: [ replaceMiddle ] };
--- arcade/the_core/05_list_forest_edge/041_replaceMiddle/js/test/index.js
+++ arcade/the_core/05_list_forest_edge/041_replaceMiddle/js/test/index.js
@@ -5,18 +5,50 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // replaceMiddle()
+      fn: '', // replaceMiddle(arr)
       ins_and_outs: [
         {
-          cxt: 'simple test',
-          in: [],
-          out: true,
+          cxt: 'with an array with an even length, the input should be returned with the two middle elements should be summed together',
+          in: [7, 2, 2, 5, 10, 7],
+          out: [7, 2, 7, 10, 7],
+        },{
+          cxt: 'with an array with an odd length, the input array should be returned',
+          in: [-5, -5, 10],
+          out: [-5, -5, 10],
+        },{
+          cxt: 'with an array with an even length, the input should be returned with the two middle elements should be summed together',
+          in: [45, 23, 12, 33, 12, 453, -234, -45],
+          out: [45, 23, 12, 45, 453, -234, -45],
+        },{
+          cxt: 'with an array with an even length, the input should be returned with the two middle elements should be summed together',
+          in: [2,8],
+          out: [10],
+        },{
+          cxt: 'with an array with an odd length, the input array should be returned',
+          in: [-12, 34, 40, -5, -12, 4, 0, 0, -12],
+          out: [-12, 34, 40, -5, -12, 4, 0, 0, -12],
+        },{
+          cxt: 'with an array with an even length, the input should be returned with the two middle elements should be summed together',
+          in: [9, 0, 15, 9],
+          out: [9, 15, 9],
+        },{
+          cxt: 'with an array with an odd length, the input array should be returned',
+          in: [-6, 6, -6],
+          out: [-6, 6, -6],
+        },{
+          cxt: 'with an array with an odd length, the input array should be returned',
+          in: [26, 26, -17],
+          out: [26, 26, -17],
+        },{
+          cxt: 'with an array with an even length, the input should be returned with the two middle elements should be summed together',
+          in: [-7, 5, 5, 10],
+          out: [-7, 10, 10],
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
