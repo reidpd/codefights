75b75fda69ad4f5cad0cbaf695d1b7aff046898b
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-04-13 16:01:29 -0600

    added 06/047

--- arcade/the_core/06_labyrinth_of_nested_loops/047_comfortableNumbers/js/index.js
+++ arcade/the_core/06_labyrinth_of_nested_loops/047_comfortableNumbers/js/index.js
@@ -1,5 +1,41 @@
-const comfortableNumbers = () => {
-  return true;
+const comfortableNumbers = (l,r) => {
+  const range = makeSegment(l,r);
+  if (range.length === 1) { return 0 }
+  const acceptable_pairs = [];
+  range.forEach((a, i) => {
+    if (i === range.length-1) { return 'loop finished'; }
+    const slice = range.slice(i+1);
+    slice.forEach((b, j) => {
+      const pair = [a,b];
+      if (aComfyWithB(...pair) && aComfyWithB(...pair.reverse())) {
+        acceptable_pairs.push(pair);
+      }
+    })
+  })
+  return acceptable_pairs.length;
 };
 
-module.exports = { fns: [ comfortableNumbers ] };
+
+const digitSum = n => n.toString().split('').map(n => parseInt(n)).reduce((acc,val) => acc + val);
+
+const makeSegment = (n,m) => {
+  if (m) { return new Array(m-n+1).fill(n).map((x,i) => x+i); }
+  let start = n - digitSum(n), end = n + digitSum(n);
+  return new Array(end - start + 1).fill(start).map((n,i) => n+i);
+}
+
+const findComfies = (n, range) => {
+  let sumOfDigits = digitSum(n), comfies = [];
+  for (let i = sumOfDigits * -1; i <= sumOfDigits; i++) {
+    if (i !== 0 && range.indexOf(n+i) > -1) { comfies.push(n + i) }
+  }
+  return comfies;
+}
+
+const aComfyWithB = (a,b) => {
+  const pair = [a,b];
+  const segment = makeSegment(a);
+  return a !== b && segment.indexOf(b) !== -1;
+}
+
+module.exports = { fns: [ comfortableNumbers, digitSum, makeSegment, aComfyWithB ] };
--- arcade/the_core/06_labyrinth_of_nested_loops/047_comfortableNumbers/js/test/index.js
+++ arcade/the_core/06_labyrinth_of_nested_loops/047_comfortableNumbers/js/test/index.js
@@ -5,13 +5,110 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // comfortableNumbers()
+      fn: '', // comfortableNumbers(l, r)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [10,12],
+          out: 2,
+        },{
+          cxt: 'simple test',
+          in: [1,9],
+          out: 20,
+        },{
+          cxt: 'simple test',
+          in: [13,13],
+          out: 0,
+        },{
+          cxt: 'simple test',
+          in: [12,108],
+          out: 707,
+        },{
+          cxt: 'complicated test',
+          in: [239,777],
+          out: 6166,
+        },{
+          cxt: 'complicated test',
+          in: [1,1000],
+          out: 11435,
         },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // digitSum(n)
+      ins_and_outs: [
+        {
+          cxt: 'simple test',
+          in: [22],
+          out: 4,
+        },{
+          cxt: 'simple test',
+          in: [10],
+          out: 1,
+        },{
+          cxt: 'simple test',
+          in: [11],
+          out: 2,
+        },{
+          cxt: 'simple test',
+          in: [1234],
+          out: 10,
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(...param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // makeSegment(n)
+      ins_and_outs: [
+        {
+          cxt: 'in conjunction with digitSum, the function should return an array containing all numbers within the range [n - dS(n), n + dS(n)], where dS(n) is the sum of the digits of n',
+          in: 22,
+          out: [18,19,20,21,22,23,24,25,26],
+        },{
+          cxt: 'the sum of the digits in 22 is 4, so the function should return a segment containing ints from (22-4) to (22+4)',
+          in: 22,
+          out: [18,19,20,21,22,23,24,25,26],
+        },{
+          cxt: 'the sum of the digits in 12 is 3, so the function should return a segment containing ints from (12-3) to (12+3)',
+          in: 12,
+          out: [9,10,11,12,13,14,15],
+        },{
+          cxt: 'the sum of the digits in 11 is 2, so the function should return a segment containing ints from (11-2) to (11+2)',
+          in: 11,
+          out: [9,10,11,12,13],
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // aComfyWithB(a,b)
+      ins_and_outs: [
+        {
+          cxt: 'returns false when the digits of a (10) do not add up to a sum (1) that can in either direction of A make a range ([9,10,11]) that includes b (12)',
+          in: [10,12],
+          out: false,
+        },{
+          cxt: 'returns true when the digits of a (12) add to a sum (3) that can in either direction of a make a range ([9,10,11,12,13,14,15]) that includes b (10)',
+          in: [12,13],
+          out: true,
+        },{
+          cxt: 'returns false if a === b',
+          in: [10, 10],
+          out: false,
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
