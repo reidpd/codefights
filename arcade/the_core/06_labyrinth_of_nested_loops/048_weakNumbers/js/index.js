dbbf6990a5902b58a497b34fc9cc3227cbba8342
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-04-13 17:40:14 -0600

    added 06/48 but still doesnt pass codefights tests exec time limit

--- arcade/the_core/06_labyrinth_of_nested_loops/048_weakNumbers/js/index.js
+++ arcade/the_core/06_labyrinth_of_nested_loops/048_weakNumbers/js/index.js
@@ -1,5 +1,27 @@
-const weakNumbers = () => {
-  return true;
+const weakNumbers = n => {
+  let maxW = 0;
+  const weaknesses = makeRange(1,n).map(x => {
+    const w = defineWeakness(x);
+    if (w > maxW) { maxW = w }
+    return w;
+  }).filter(x => x === maxW);
+  return [maxW, weaknesses.length];
 };
 
-module.exports = { fns: [ weakNumbers ] };
+const defineWeakness = x => {
+  if (x === 1) { return 0 }
+  const x_divs = findDivisors(x).length;
+  const div_map = makeRange(1,x-1).map(n => findDivisors(n).length);
+  return div_map.filter(n_divs => n_divs > x_divs).length;
+}
+
+const makeRange = (a,b) => new Array(b-a+1).fill(a).map((x,i) => x+i);
+
+const findDivisors = (n, divisors = []) => {
+  makeRange(1,n).forEach(int => {
+    if (n % int === 0) { divisors.push(int) }
+  });
+  return divisors;
+}
+
+module.exports = { fns: [ weakNumbers, findDivisors, defineWeakness, makeRange ] };
--- arcade/the_core/06_labyrinth_of_nested_loops/048_weakNumbers/js/test/index.js
+++ arcade/the_core/06_labyrinth_of_nested_loops/048_weakNumbers/js/test/index.js
@@ -5,13 +5,130 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // weakNumbers()
+      fn: '', // weakNumbers(n)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: 9,
+          out: [2,2],
+        },{
+          cxt: 'simple test',
+          in: 1,
+          out: [0,1],
+        },{
+          cxt: 'simple test',
+          in: 2,
+          out: [0,2],
+        },{
+          cxt: 'simple test',
+          in: 7,
+          out: [2,1],
+        },{
+          cxt: 'simple test',
+          in: 500,
+          out: [403,1],
+        },{
+          cxt: 'simple test',
+          in: 4,
+          out: [0,4],
+        },{
+          cxt: 'simple test',
+          in: 1000,
+          out: [0,4],
         },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // findDivisors(n)
+      ins_and_outs: [
+        {
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 1,
+          out: [1],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 2,
+          out: [1,2],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 3,
+          out: [1,3],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 4,
+          out: [1,2,4],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 5,
+          out: [1,5],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 6,
+          out: [1,2,3,6],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 7,
+          out: [1,7],
+        },{
+          cxt: 'returns an array containing all integers that are common divisors with n',
+          in: 8,
+          out: [1,2,4,8],
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // defineWeakness(x)
+      ins_and_outs: [
+        {
+          cxt: 'returns the amount of integers less than x that have more divisors than x',
+          in: 1,
+          out: 0,
+        },{
+          cxt: 'returns the amount of integers less than x that have more divisors than x',
+          in: 5,
+          out: 1,
+        },{
+          cxt: 'returns the amount of integers less than x that have more divisors than x',
+          in: 7,
+          out: 2,
+        },{
+          cxt: 'returns the amount of integers less than x that have more divisors than x',
+          in: 9,
+          out: 2,
+        },{
+          cxt: 'returns the amount of integers less than x that have more divisors than x',
+          in: 4,
+          out: 0,
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // makeRange(a,b)
+      ins_and_outs: [
+        {
+          cxt: 'returns an array of consecutive integers from a to b',
+          in: [1,10],
+          out: [1,2,3,4,5,6,7,8,9,10],
+        },{
+          cxt: 'returns an array of consecutive integers from a to b',
+          in: [2,5],
+          out: [2,3,4,5],
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
