7c86cebff05bbe6a75c61aa59e4d906ee5a99ecb
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-04-19 11:18:24 -0700

    added 07/057

--- arcade/the_core/07_book_market/057_isMAC48Address/js/index.js
+++ arcade/the_core/07_book_market/057_isMAC48Address/js/index.js
@@ -1,5 +1,7 @@
-const isMAC48Address = () => {
-  return true;
+const isMAC48Address = inputString => {
+  const split = inputString.split('-');
+  return split.length === 6 && split.every(pair =>
+    pair.length === 2 && pair.split('').every(char => !isNaN(parseInt(char, 16))));
 };
 
 module.exports = { fns: [ isMAC48Address ] };
--- arcade/the_core/07_book_market/057_isMAC48Address/js/test/index.js
+++ arcade/the_core/07_book_market/057_isMAC48Address/js/test/index.js
@@ -5,18 +5,54 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // isMAC48Address()
+      fn: '', // isMAC48Address(inputString)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
+          in: "00-1B-63-84-45-E6",
           out: true,
+        },{
+          cxt: 'simple test',
+          in: "Z1-1B-63-84-45-E6",
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: "not a MAC-48 address",
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: "FF-FF-FF-FF-FF-FF",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "00-00-00-00-00-00",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "G0-00-00-00-00-00",
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: "02-03-04-05-06-07-",
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: "12-34-56-78-9A-BC",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "FF-FF-AB-CD-EA-BC",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "12-34-56-78-9A-BG",
+          out: false,
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
