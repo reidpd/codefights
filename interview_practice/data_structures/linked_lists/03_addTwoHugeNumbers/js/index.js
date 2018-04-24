3e4cf5e151b27c6b8803c404372a348f09770a87
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-04-24 12:29:01 -0700

    restarted linked lists 03 but need to account for extreme nums to continue problem later

--- interview_practice/data_structures/linked_lists/03_addTwoHugeNumbers/js/index.js
+++ interview_practice/data_structures/linked_lists/03_addTwoHugeNumbers/js/index.js
@@ -1,5 +1,36 @@
-const addTwoHugeNumbers = () => {
-  return true;
+const createListFromArray = require('../../00_basic/lists/single/js/index.js').createListFromArray;
+
+const addTwoHugeNumbers = (a,b) => { // a && b === linked lists
+  let num_lists = [a,b], total = 0;
+  let big_num_strings = num_lists.map((list, i) => {
+    let node = list, str = "";
+    while (node !== null) {
+      let n = node.value.toString();
+      if (str.length !== 0) { n = n.padStart(4, '0') }
+      str += n;
+      node = node.next;
+    }
+    return str;
+  });
+  const max_length = Math.max(...big_num_strings.map(str => str.length));
+  big_num_strings = big_num_strings.map(str => str.padStart(max_length, '0').split('').reverse());
+  console.log(big_num_strings);
+  for (let pad_len = 0; pad_len < max_length; pad_len++) {
+    let digits = big_num_strings.map(n => n[pad_len].padEnd(pad_len+1, '0'));
+    console.log(digits);
+    // let rounded_below_tenkay = digits.map(n => {
+    //   if (!n.includes('.')) { n += '.0' }
+    //   while (n.split('.')[0].length > 4) {
+    //     n = parseInt(n / 10).toString();
+    //   }
+    //   return n;
+    // })
+    // console.log('rounded_below_tenkay === ', rounded_below_tenkay);
+    digits.forEach(n => total += parseInt(n, 10));
+    console.log(total);
+  }
+  return total.toString().split('e');
+
 };
 
 module.exports = { fns: [ addTwoHugeNumbers ] };
--- interview_practice/data_structures/linked_lists/03_addTwoHugeNumbers/js/test/index.js
+++ interview_practice/data_structures/linked_lists/03_addTwoHugeNumbers/js/test/index.js
@@ -1,16 +1,69 @@
 const expect = require('chai').expect;
 const fns = require('..').fns;
+const createListFromArray = require('../../../00_basic/lists/single/js').createListFromArray;
 console.log('fns === ', fns);
 
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // addTwoHugeNumbers()
+      fn: '', // addTwoHugeNumbers(a,b)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [
+            createListFromArray([9876, 5432, 1999]),
+            createListFromArray([1, 8001]),
+          ],
+          out: createListFromArray([9876, 5434, 0]),
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([123, 4, 5]),
+            createListFromArray([100, 100, 100]),
+          ],
+          out: createListFromArray([223, 104, 105]),
+        },{
+          cxt: 'arrs filled with only a single 0 returns the input given',
+          in: [
+            createListFromArray([0]),
+            createListFromArray([0]),
+          ],
+          out: createListFromArray([0]),
+        },{
+          cxt: 'adding 0 to the first num given returns the first num',
+          in: [
+            createListFromArray([1234, 1234, 0]),
+            createListFromArray([0]),
+          ],
+          out: createListFromArray([1234, 1234, 0]),
+        },{
+          cxt: 'adding the second num to 0 returns the second num',
+          in: [
+            createListFromArray([0]),
+            createListFromArray([1234, 123, 0]),
+          ],
+          out: createListFromArray([1234, 123, 0]),
+        },{
+          cxt: 'adding one to a very long num that only needs one to shorten itself, will return the shorter num with following zeros',
+          in: [
+            createListFromArray([1]),
+            createListFromArray([9998, 9999, 9999, 9999, 9999, 9999]),
+          ],
+          out: createListFromArray([9999, 0, 0, 0, 0, 0]),
+        },{
+          cxt: 'adding one to a very long num that only needs one to shorten itself, will return the shorter num with following zeros',
+          in: [
+            createListFromArray([1]),
+            createListFromArray([9999, 9999, 9999, 9999, 9999, 9999]),
+          ],
+          out: createListFromArray([1, 0, 0, 0, 0, 0, 0]),
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([8339, 4510]),
+            createListFromArray([2309]),
+          ],
+          out: createListFromArray([8339, 6819]),
         },
       ],
       run: param => {
