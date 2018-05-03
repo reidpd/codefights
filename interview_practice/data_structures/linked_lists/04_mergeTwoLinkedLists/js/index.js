86551008aa127333e99fb35477fc85d9941d3f0a
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-05-03 15:32:16 -0700

    added & finished linked_lists/04

--- interview_practice/data_structures/linked_lists/04_mergeTwoLinkedLists/js/index.js
+++ interview_practice/data_structures/linked_lists/04_mergeTwoLinkedLists/js/index.js
@@ -1,5 +1,21 @@
-const mergeTwoLinkedLists = () => {
-  return true;
+const mergeTwoLinkedLists = (l1, l2) => {
+  let merge = [];
+  while (true) {
+    if (l1 === null && l2 === null) { break }
+    if (l1 === null && l2 !== null) { merge.push(l2.value); l2 = l2.next; }
+    else if (l1 !== null && l2 === null) { merge.push(l1.value); l1 = l1.next; }
+    else {
+      let next_value = Math.min(l1.value, l2.value);
+      if (next_value === l1.value) {
+        merge.push(l1.value);
+        l1 = l1.next;
+      } else if (next_value === l2.value) {
+        merge.push(l2.value);
+        l2 = l2.next;
+      }
+    }
+  }
+  return merge;
 };
 
 module.exports = { fns: [ mergeTwoLinkedLists ] };
--- interview_practice/data_structures/linked_lists/04_mergeTwoLinkedLists/js/test/index.js
+++ interview_practice/data_structures/linked_lists/04_mergeTwoLinkedLists/js/test/index.js
@@ -1,16 +1,97 @@
 const expect = require('chai').expect;
 const fns = require('..').fns;
+const createListFromArray = require('../../../00_basic/lists/single/js').createListFromArray;
 console.log('fns === ', fns);
 
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // mergeTwoLinkedLists()
+      fn: '', // mergeTwoLinkedLists(l1, l2)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [
+            createListFromArray([1,2,3]),
+            createListFromArray([4,5,6])
+          ],
+          out: [1,2,3,4,5,6],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([1, 1, 2, 4]),
+            createListFromArray([0, 3, 5])
+          ],
+          out: [0, 1, 1, 2, 3, 4, 5],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([5, 10, 15, 40]),
+            createListFromArray([2, 3, 20])
+          ],
+          out: [2, 3, 5, 10, 15, 20, 40],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([1,1]),
+            createListFromArray([2,4])
+          ],
+          out: [1,1,2,4],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([]),
+            createListFromArray([1, 1, 2, 2, 4, 7, 7, 8])
+          ],
+          out: [1, 1, 2, 2, 4, 7, 7, 8],
+        },{
+          cxt: 'returns an empty list if given empty lists as input',
+          in: [
+            createListFromArray([]),
+            createListFromArray([])
+          ],
+          out: [],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([1,1,4]),
+            createListFromArray([])
+          ],
+          out: [1,1,4],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([1,1]),
+            createListFromArray([0])
+          ],
+          out: [0,1,1],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([0]),
+            createListFromArray([2])
+          ],
+          out: [0,2],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([1]),
+            createListFromArray([-1000000000, 1000000000])
+          ],
+          out: [-1000000000, 1, 1000000000],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([-1, -1, 0, 1]),
+            createListFromArray([-1, 0, 0, 1, 1])
+          ],
+          out: [-1, -1, -1, 0, 0, 0, 1, 1, 1],
+        },{
+          cxt: 'simple test',
+          in: [
+            createListFromArray([-780990573, -670826849, -404817961, 242026249, 731519938]),
+            createListFromArray([-815817641, -426491047, 437929670, 520408640])
+          ],
+          out: [-815817641, -780990573, -670826849, -426491047, -404817961, 242026249, 437929670, 520408640, 731519938],
         },
       ],
       run: param => {
