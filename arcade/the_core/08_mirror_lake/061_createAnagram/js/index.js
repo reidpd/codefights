517ad07765c06b48e9d36e53a3d7fd342ab623d1
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-05-07 20:45:58 -0700

    finished 08/061

--- arcade/the_core/08_mirror_lake/061_createAnagram/js/index.js
+++ arcade/the_core/08_mirror_lake/061_createAnagram/js/index.js
@@ -1,6 +1,11 @@
 const createAnagram = (s,t) => {
-  s = s.split(''), t = t.split('');
-  
+  t = t.split(''), s = s.split('');
+  for (let i = 0; i < s.length; i++) {
+    if (t.indexOf(s[i]) > -1) {
+      t[t.indexOf(s[i])] = '+';
+    } else { s[i] = '_' }
+  }
+  return s.filter(char => char === '_').length;
 };
 
 module.exports = { fns: [ createAnagram ] };
