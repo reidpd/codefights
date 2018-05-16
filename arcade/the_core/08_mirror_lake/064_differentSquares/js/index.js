62532d907a5f63631613ebe269f0c7a836340e43
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-05-16 13:05:51 -0700

    cache previous work

--- arcade/the_core/08_mirror_lake/064_differentSquares/js/index.js
+++ arcade/the_core/08_mirror_lake/064_differentSquares/js/index.js
@@ -1,6 +1,6 @@
 const differentSquares = matrix => {
   if (matrix.length < 2 || matrix.every(row => row.length < 2)) { return 0; }
-  const variations = [];
+  const variations = {};
   for (let firstRowIdx = 0; firstRowIdx < matrix.length-1; firstRowIdx++) {
     let secondRowIdx = firstRowIdx + 1;
     for (let colA = 0; colA < matrix[firstRowIdx].length - 1; colA++) {
@@ -8,16 +8,18 @@ const differentSquares = matrix => {
       let a = matrix[firstRowIdx][colA], b = matrix[firstRowIdx][colB],
           c = matrix[secondRowIdx][colA], d = matrix[secondRowIdx][colB];
       if (a && b && c && d) {
-        let square = [[a,b],[c,d]];
-        if (!variations.some((storedSquare, i) => {
-          return storedSquare.every((row, rowIdx) => {
-            return row.every((col, colIdx) => {
-              return storedSquare[rowIdx][colIdx] === square[rowIdx][colIdx];
-            })
-          })
-        })) {
-          variations.push(square);
-        }
+        // let square = [[a,b],[c,d]];
+        // if (!variations.some((storedSquare, i) => {
+        //   return storedSquare.every((row, rowIdx) => {
+        //     return row.every((col, colIdx) => {
+        //       return storedSquare[rowIdx][colIdx] === square[rowIdx][colIdx];
+        //     })
+        //   })
+        // })) {
+        //   variations.push(square);
+        // }
+        let square = "" + a + b + c + d;
+        if (variations.indexOf(square) === -1) { variations.push(square) }
       }
     }
   }
