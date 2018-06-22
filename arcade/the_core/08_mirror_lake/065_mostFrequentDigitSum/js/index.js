8c647fabf8e38252cd9ce3473c63d4c4d24b9aca
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-06-21 21:20:56 -0700

    solution for 08/065

--- arcade/the_core/08_mirror_lake/065_mostFrequentDigitSum/js/index.js
+++ arcade/the_core/08_mirror_lake/065_mostFrequentDigitSum/js/index.js
@@ -1,5 +1,34 @@
-const funcName = () => {
-  return true;
+const mostFrequentDigitSum = n => {
+  const sequence = makeSequence(n).map(int => sumOfDigits(int));
+  let counts = {};
+  sequence.forEach(int => {
+    if (!counts[int]) { counts[int] = 1 } else { counts[int]++ }
+  });
+  let keys = Object.keys(counts), maxCount = 0;
+  keys.forEach(int => {
+    if (counts[int] > maxCount) { maxCount = counts[int] }
+  });
+  let frequents = [];
+  keys.forEach(int => {
+    if (counts[int] === maxCount) { frequents.push(parseInt(int)) }
+  });
+  return Math.max(...frequents);
 };
 
-module.exports = { fns: [ funcName ] };
+const makeSequence = n => {
+    let sequence = [n];
+    while (sequence[sequence.length-1] > 0) {
+        let digitsSum = sumOfDigits(n);
+        n -= digitsSum;
+        sequence.push(n);
+    }
+    return sequence;
+}
+
+const sumOfDigits = n => {
+    let digitSplit = n.toString().split('').map(str => parseInt(str));
+    return digitSplit.reduce((a,b)=>a+b);
+};
+
+
+module.exports = { fns: [ mostFrequentDigitSum, makeSequence ] };
--- arcade/the_core/08_mirror_lake/065_mostFrequentDigitSum/js/test/index.js
+++ arcade/the_core/08_mirror_lake/065_mostFrequentDigitSum/js/test/index.js
@@ -5,18 +5,61 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // mostFrequentDigitSum(n)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: 88,
+          out: 9,
+        },{
+          cxt: 'simple test',
+          in: 8,
+          out: 8,
+        },{
+          cxt: 'simple test',
+          in: 1,
+          out: 1,
+        },{
+          cxt: 'simple test',
+          in: 17,
+          out: 9,
+        },{
+          cxt: 'simple test',
+          in: 239,
+          out: 9,
+        },{
+          cxt: 'simple test',
+          in: 994,
+          out: 9,
+        },{
+          cxt: 'simple test',
+          in: 99999,
+          out: 18,
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
+        });
+      }
+    }, {
+      fn: '', // makeSequence(n)
+      ins_and_outs: [
+        {
+          cxt: 'simple test',
+          in: 88,
+          out: [88, 72, 63, 54, 45, 36, 27, 18, 9, 0],
+        },{
+          cxt: 'simple test',
+          in: 8,
+          out: [8, 0],
+        },
+      ],
+      run: param => {
+        const cxt = param.fn.name + '() ' + param.cxt;
+        it(cxt, () => {
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
