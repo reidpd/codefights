6e84f3e45096a0da51f585bd7824a26b09e44428
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-06-28 11:57:17 -0700

    js solution for 09/074

--- arcade/the_core/09_well_of_integration/074_timedReading/js/index.js
+++ arcade/the_core/09_well_of_integration/074_timedReading/js/index.js
@@ -1,5 +1,10 @@
-const funcName = () => {
-  return true;
+const timedReading = (maxLength, text) => {
+  const words = text.toLowerCase().split('').map(char => {
+    let cp = char.codePointAt(0);
+    if (cp === 32 || (96 < cp && cp < 123)) { return char }
+    else { return '*' }
+  }).filter(char => char !== '*').join('').split(' ');
+  return words.filter(w => w.length !== 0 && w.length <= maxLength).length;
 };
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ timedReading ] };
--- arcade/the_core/09_well_of_integration/074_timedReading/js/test/index.js
+++ arcade/the_core/09_well_of_integration/074_timedReading/js/test/index.js
@@ -5,12 +5,64 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // timedReading(maxLength, text)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [
+            4,
+            "The Fox asked the stork, 'How is the soup?'"
+          ],
+          out: 7,
+        },{
+          cxt: 'simple test',
+          in: [
+            1,
+            "..."
+          ],
+          out: 0,
+        },{
+          cxt: 'simple test',
+          in: [
+            3,
+            "This play was good for us."
+          ],
+          out: 3,
+        },{
+          cxt: 'simple test',
+          in: [
+            3,
+            "Suddenly he stopped, and glanced up at the houses"
+          ],
+          out: 5,
+        },{
+          cxt: 'simple test',
+          in: [
+            6,
+            "Zebras evolved among the Old World horses within the last four million years."
+          ],
+          out: 11,
+        },{
+          cxt: 'simple test',
+          in: [
+            5,
+            "Although zebra species may have overlapping ranges, they do not interbreed."
+          ],
+          out: 6,
+        },{
+          cxt: 'simple test',
+          in: [
+            1,
+            "Oh!"
+          ],
+          out: 0,
+        },{
+          cxt: 'simple test',
+          in: [
+            5,
+            "Now and then, however, he is horribly thoughtless, and seems to take a real delight in giving me pain."
+          ],
+          out: 14,
         },
       ],
       run: param => {
