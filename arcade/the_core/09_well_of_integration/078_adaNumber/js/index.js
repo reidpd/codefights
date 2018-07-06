58a674892ffd928e2ecf4420f56f131b005f85f1
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-06 15:02:29 -0700

    initial tackle of the_core/09/078

--- arcade/the_core/09_well_of_integration/078_adaNumber/js/index.js
+++ arcade/the_core/09_well_of_integration/078_adaNumber/js/index.js
@@ -1,5 +1,16 @@
-const funcName = () => {
-  return true;
+const adaNumber = line => {
+  line = line.filter(char => char !== '_');
+  console.log(line);
+  return followsRepOne(line) || followsRepTwo(line);
 };
 
-module.exports = { fns: [ funcName ] };
+const followsRepOne = line => {
+  return line.length > 0 && line.split('').every(char => !isNaN(char));
+};
+
+const followsRepTwo = line => {
+  for (let i = 2)
+  return false;
+}
+
+module.exports = { fns: [ adaNumber ] };
--- arcade/the_core/09_well_of_integration/078_adaNumber/js/test/index.js
+++ arcade/the_core/09_well_of_integration/078_adaNumber/js/test/index.js
@@ -5,18 +5,86 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // adaNumber(line)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
+          in: "123_456_789",
           out: true,
-        },
+        },{
+          cxt: 'simple test',
+          in: "16#123abc#",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "10#123abc#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "10#10#123ABC#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "10#0#",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "10##",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "16#1234567890ABCDEFabcdef#",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "1600#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "7#???#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "4#4#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "3454235ab534",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "1#0#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "17#ac#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "2#10110#",
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: "2#10110",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "#2#10110",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "#2#10110#",
+          out: !true,
+        },{
+          cxt: 'simple test',
+          in: "9##",
+          out: false
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
