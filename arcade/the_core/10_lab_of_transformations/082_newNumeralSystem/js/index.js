a257a49367afac9f4a1b8f3496a2672e97bd9924
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-07 16:19:15 -0700

    js solution for the_core/10/082

--- arcade/the_core/10_lab_of_transformations/082_newNumeralSystem/js/index.js
+++ arcade/the_core/10_lab_of_transformations/082_newNumeralSystem/js/index.js
@@ -1,5 +1,13 @@
-const funcName = () => {
-  return true;
+const newNumeralSystem = number => {
+  let result = [], letters = [number], charCode = number.charCodeAt(0) - 1;
+  while (charCode >= 65) { letters.unshift(String.fromCharCode(charCode--)); }
+  while (letters.length > 0) {
+    const pair = [letters.shift()];
+    if (letters.length === 0) { pair[1] = pair[0] }
+    else { pair.push(letters.pop()) }
+    result.push(pair)
+  }
+  return result.map(pair => pair[0] + " + " + pair[1]);
 };
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ newNumeralSystem ] };
--- arcade/the_core/10_lab_of_transformations/082_newNumeralSystem/js/test/index.js
+++ arcade/the_core/10_lab_of_transformations/082_newNumeralSystem/js/test/index.js
@@ -5,12 +5,60 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // newNumeralSystem
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: 'G',
+          out: ["A + G", "B + F", "C + E", "D + D"],
+        },{
+          cxt: 'simple test',
+          in: 'A',
+          out: ['A + A'],
+        },{
+          cxt: 'simple test',
+          in: 'D',
+          out: ["A + D", "B + C"],
+        },{
+          cxt: 'simple test',
+          in: 'E',
+          out: ["A + E", "B + D", "C + C"],
+        },{
+          cxt: 'simple test',
+          in: 'F',
+          out: ["A + F", "B + E", "C + D"],
+        },{
+          cxt: 'simple test',
+          in: 'I',
+          out: ["A + I", "B + H", "C + G", "D + F", "E + E"],
+        },{
+          cxt: 'simple test',
+          in: 'K',
+          out: ["A + K", "B + J", "C + I", "D + H", "E + G", "F + F"],
+        },{
+          cxt: 'simple test',
+          in: 'L',
+          out: ["A + L", "B + K", "C + J", "D + I", "E + H", "F + G"],
+        },{
+          cxt: 'simple test',
+          in: 'O',
+          out: ["A + O", "B + N", "C + M", "D + L", "E + K", "F + J", "G + I", "H + H"],
+        },{
+          cxt: 'simple test',
+          in: 'P',
+          out: ["A + P", "B + O", "C + N", "D + M", "E + L", "F + K", "G + J", "H + I"],
+        },{
+          cxt: 'simple test',
+          in: 'S',
+          out: ["A + S", "B + R", "C + Q", "D + P", "E + O", "F + N", "G + M", "H + L", "I + K", "J + J"],
+        },{
+          cxt: 'simple test',
+          in: 'T',
+          out: ["A + T", "B + S", "C + R", "D + Q", "E + P", "F + O", "G + N", "H + M", "I + L", "J + K"],
+        },{
+          cxt: 'simple test',
+          in: 'W',
+          out: ["A + W", "B + V", "C + U", "D + T", "E + S", "F + R", "G + Q", "H + P", "I + O", "J + N", "K + M", "L + L"],
         },
       ],
       run: param => {
