81ca92e36714fcee8a6942c52a582b9eaa04e671
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-11 11:53:59 -0700

    js solution for the_core/10/085

--- arcade/the_core/10_lab_of_transformations/085_higherVersion/js/index.js
+++ arcade/the_core/10_lab_of_transformations/085_higherVersion/js/index.js
@@ -1,5 +1,13 @@
-const funcName = () => {
-  return true;
-};
+const higherVersion = (ver1, ver2) => {
+    const versions = [ver1,ver2].map(ver => ver.split('.').map(str => parseInt(str, 10)));
+    let v1 = versions[0], v2 = versions[1];
+    for (let i = 0; i < v1.length; i++) {
+        if (v1[i] !== v2[i]) {
+            return v1[i] > v2[i];
+        }
+    }
+    return false;
+}
 
-module.exports = { fns: [ funcName ] };
+
+module.exports = { fns: [ higherVersion ] };
--- arcade/the_core/10_lab_of_transformations/085_higherVersion/js/test/index.js
+++ arcade/the_core/10_lab_of_transformations/085_higherVersion/js/test/index.js
@@ -5,12 +5,85 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // higherVersion(ver1, ver2)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
+          in: [
+            "1.2.2",
+            "1.2.0"
+          ],
           out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.0.5",
+            "1.1.0"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.1.0",
+            "1.1.5"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "10",
+            "9"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.0.10",
+            "1.1.5"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "5",
+            "1"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.1.10",
+            "1.2.0"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.2.2",
+            "1.2.10"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "1.10.2",
+            "1.2.10"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "0",
+            "0"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "4.3.22.1",
+            "4.3.22.1"
+          ],
+          out: false,
         },
       ],
       run: param => {
