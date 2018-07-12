e0f5ceafdf92c77df20d239f506a61355d798bd5
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-12 09:50:02 -0700

    js solution for the_core/10/86

--- arcade/the_core/10_lab_of_transformations/086_decipher/js/index.js
+++ arcade/the_core/10_lab_of_transformations/086_decipher/js/index.js
@@ -1,5 +1,14 @@
-const funcName = () => {
-  return true;
-};
+const decipher = cipher => {
+    let initialStr = [], nums = [], ciph = cipher.split('');
+    const parse = ints => parseInt(ints.join(''),10);
+    while (ciph.length > 0) {
+        nums.push(ciph.shift());
+        if (96 < parse(nums) && parse(nums) < 123) {
+            initialStr.push(String.fromCharCode(parse(nums)));
+            nums = [];
+        }
+    }
+    return initialStr.join('');
+}
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ decipher ] };
--- arcade/the_core/10_lab_of_transformations/086_decipher/js/test/index.js
+++ arcade/the_core/10_lab_of_transformations/086_decipher/js/test/index.js
@@ -5,12 +5,36 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // decipher(cipher)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: "10197115121",
+          out: "easy",
+        },{
+          cxt: 'simple test',
+          in: "98",
+          out: "b",
+        },{
+          cxt: 'simple test',
+          in: "122",
+          out: "z",
+        },{
+          cxt: 'simple test',
+          in: "1229897",
+          out: "zba",
+        },{
+          cxt: 'simple test',
+          in: "97989910010110210310410510610710810911011111211311411511611711811912012112297",
+          out: "abcdefghijklmnopqrstuvwxyza",
+        },{
+          cxt: 'simple test',
+          in: "10297115106108102108971061151041029897107106115981001029710711510010298",
+          out: "fasjlflajshfbakjsbdfaksdfb",
+        },{
+          cxt: 'simple test',
+          in: "11211111911310110810910097107108115111112119113101106107971101021101061021041149710511411497",
+          out: "powqelmdaklsopwqejkanfnjfhrairra",
         },
       ],
       run: param => {
