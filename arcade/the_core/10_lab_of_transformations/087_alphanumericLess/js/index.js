c65720252d125f62ed373c3d7f5a24dc12430bf6
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-15 15:28:11 -0700

    first try at 10/87

--- arcade/the_core/10_lab_of_transformations/087_alphanumericLess/js/index.js
+++ arcade/the_core/10_lab_of_transformations/087_alphanumericLess/js/index.js
@@ -1,5 +1,70 @@
-const funcName = () => {
-  return true;
+const alphanumericLess = (s1, s2) => {
+    // return true if s1 is alphanumerically less than s2, otherwise false
+    const seqs = [s1, s2].map(s => findTokenSeq(s));
+    let compareBools = [];
+    while (seqs.every(s => s.length > 0)) {
+        let tokens = seqs.slice().map(s => s.shift());
+        compareBools.push(compareTokens(...tokens))
+        console.log('tokens === ', tokens)
+        console.log('seqs === ', seqs)
+    }
+    console.log('compareBools === ', compareBools);
+    if (seqs[0].length !== seqs[1].length) { return seqs[0].length < seqs[1].length }
+    console.log(seqs);
+    return compareBools.every(b => b);
 };
 
-module.exports = { fns: [ funcName ] };
+const compareTokens = (a,b) => {
+  if (symbolsMatchExpectation(a, b,'letter')) { return a < b }
+  else if (symbolsMatchExpectation(a, b, 'digit')) {
+    let leadingZeros = [a,b].map(num => {
+      let digits = num.split('').slice(), count = 0;
+      while (digits.shift() === '0') { count++ }
+      return count;
+    });
+    const digits = tokens.map(t => parseInt(t,10));
+    if (digits[0] === digits[1]) {
+      return leadingZeros[0] < leadingZeros[1]);
+    } else {
+      return digits[0] < digits[1];
+    }
+  } else {
+    return symbolIdentity(a) === 'digit' && symbolIdentity(b) === 'letter';
+  }
+}
+
+const findTokenSeq = str => {
+    const s = str.split('');
+    let seq = [], stack = [];
+    while (s.length > 0) {
+        const char = s.shift();
+        stack.push(char);
+        if (isDigit(char)) {
+            while (s.length !== 0 && isDigit(s[0])) {
+                stack.push(s.shift());
+            }
+        }
+        seq.push(stack.join(''));
+        stack = [];
+    }
+    return seq;
+}
+
+
+const isDigit = str => !isNaN(parseInt(str));
+
+const bothDigits = (sym1, sym2) => {
+    const bools = [sym1,sym2].map(s => !isNaN(parseInt(s)));
+    return bools[0] === bools[1];
+}
+
+const symbolIdentity = s => isNaN(parseInt(s)) ? 'letter' : 'digit';
+
+const symbolsMatchExpectation = (sym1, sym2, expectation) => [sym1,sym2].map(s => symbolIdentity(s)).every(id => id === expectation);
+
+const bothLetters = (sym1, sym2) => {
+    return [sym1, sym2].map(s => symbolIdentity(s)).every(id => id === 'letter');
+}
+
+
+module.exports = { fns: [ alphanumericLess ] };
--- arcade/the_core/10_lab_of_transformations/087_alphanumericLess/js/test/index.js
+++ arcade/the_core/10_lab_of_transformations/087_alphanumericLess/js/test/index.js
@@ -5,13 +5,93 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // alphanumericLess(s1, s2)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
+          in: [
+            "a",
+            "a1"
+          ],
           out: true,
-        },
+        },{
+          cxt: 'simple test',
+          in: [
+            "ab",
+            "a1"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "b",
+            "a1"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "x11y012",
+            "x011y13"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "ab000144",
+            "ab144"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "ab",
+            "a"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "000",
+            "0000"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "abc123",
+            "abc123"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "zza1233",
+            "zza1234"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "zzz1",
+            "zzz1"
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "00",
+            "a2"
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "000000",
+            "a2"
+          ],
+          out: true,
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
