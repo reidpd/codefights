0f44bc47193f5143774ab236338ab99ada49200d
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-17 17:53:53 -0700

    js solution for the_core/11/089

--- arcade/the_core/11_spring_of_integration/089_arrayPreviousLess/js/index.js
+++ arcade/the_core/11_spring_of_integration/089_arrayPreviousLess/js/index.js
@@ -1,5 +1,17 @@
-const funcName = () => {
-  return true;
-};
+const arrayPreviousLess = items => {
+    const slice = items.slice();
+    for (let i = 0; i < items.length; i++) {
+        let lesserItemValue = -1;
+        for (let j = i-1; j > -1; j--) {
+            if (items[j] < items[i] && items[j] !== -1) {
+                lesserItemValue = items[j];
+                break;
+            }
+        }
+        slice[i] = lesserItemValue;
+    }
+    return slice;
+}
+
 
 module.exports = { fns: [ funcName ] };
--- arcade/the_core/11_spring_of_integration/089_arrayPreviousLess/js/test/index.js
+++ arcade/the_core/11_spring_of_integration/089_arrayPreviousLess/js/test/index.js
@@ -5,18 +5,38 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // arrayPreviousLess(items)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [3, 5, 2, 4, 5],
+          out: [-1, 3, -1, 2, 4],
+        },{
+          cxt: 'simple test',
+          in: [2, 2, 1, 3, 4, 5, 5, 3],
+          out: [-1, -1, -1, 1, 3, 4, 4, 1],
+        },{
+          cxt: 'simple test',
+          in: [3, 2, 1],
+          out: [-1, -1, -1],
+        },{
+          cxt: 'simple test',
+          in: [100, 101, 102],
+          out: [-1, 100, 101],
+        },{
+          cxt: 'simple test',
+          in: [7, 7, 8, 3, 4, 4, 2, 5, 6, 7, 7],
+          out: [-1, -1, 7, -1, 3, 3, -1, 2, 5, 6, 6],
+        },{
+          cxt: 'simple test',
+          in: [68, 135, 101, 170, 125],
+          out: [-1, 68, 68, 101, 101],
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
