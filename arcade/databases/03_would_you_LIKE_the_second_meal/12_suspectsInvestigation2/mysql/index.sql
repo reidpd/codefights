9af5d1205e63141d008629479069f4bb411e41c7
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-18 15:17:11 -0700

    mysql solution for databases/03/12

--- arcade/databases/03_would_you_LIKE_the_second_meal/12_suspectsInvestigation2/mysql/index.sql
+++ arcade/databases/03_would_you_LIKE_the_second_meal/12_suspectsInvestigation2/mysql/index.sql
@@ -1,5 +1,12 @@
 /*Please add ; after each select statement*/
-CREATE PROCEDURE procedureName()
+CREATE PROCEDURE suspectsInvestigation2()
 BEGIN
+
+SELECT id, name, surname
+FROM Suspect
+WHERE height < 171
+OR LOWER(name) NOT LIKE 'b%'
+OR LOWER(surname) NOT LIKE 'gre_n'
+ORDER BY id;
 
 END
