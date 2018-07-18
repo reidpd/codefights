da06e3d7016a8452618383448834f4f475873a35
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-17 17:37:14 -0700

    js solution to the_core/11/088

--- arcade/the_core/11_spring_of_integration/088_arrayConversion/js/index.js
+++ arcade/the_core/11_spring_of_integration/088_arrayConversion/js/index.js
@@ -1,5 +1,34 @@
-const funcName = () => {
-  return true;
-};
+const arrayConversion = inputArray => {
+    let i = 0;
+    while (inputArray.length > 1) {
+        inputArray = i++ % 2 === 0 ? replace(inputArray, 'sum') : replace(inputArray, 'product');
+    }
+    return inputArray[0];
+}
 
-module.exports = { fns: [ funcName ] };
+const replace = (arr, method) => {
+    const newArr = [];
+    const obj = separateByModulo(arr);
+    for (let i = 0; i < obj.even.length; i++) {
+        if (method === 'sum') { newArr.push(sum(obj,i)); }
+        else if (method === 'product') { newArr.push(product(obj,i)); }
+    }
+    return newArr;
+}
+
+const sum = (obj, i) => obj.even[i] + obj.odd[i];
+const product = (obj, i) => obj.even[i] * obj.odd[i];
+
+const separateByModulo = arr => {
+    let even = [], odd = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            even.push(arr[i]);
+        } else {
+            odd.push(arr[i]);
+        }
+    }
+    return { even, odd };
+}
+
+module.exports = { fns: [ arrayConversion ] };
--- arcade/the_core/11_spring_of_integration/088_arrayConversion/js/test/index.js
+++ arcade/the_core/11_spring_of_integration/088_arrayConversion/js/test/index.js
@@ -5,18 +5,26 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // arrayConversion(inputArray)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
-          out: true,
+          in: [1, 2, 3, 4, 5, 6, 7, 8],
+          out: 186,
+        },{
+          cxt: 'simple test',
+          in: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+          out: 64,
+        },{
+          cxt: 'simple test',
+          in: [3, 3, 5, 5],
+          out: 60,
         },
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
         it(cxt, () => {
-          expect(param.fn(...param.in)).to.deep.equal(param.out);
+          expect(param.fn(param.in)).to.deep.equal(param.out);
         });
       }
     }/*, {
