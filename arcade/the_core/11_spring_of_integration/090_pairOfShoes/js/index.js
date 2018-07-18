afbd13a935a3e76fe948cf98acbdcb42b4be55fc
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-17 18:13:07 -0700

    js solution for the_core/11/090

--- arcade/the_core/11_spring_of_integration/090_pairOfShoes/js/index.js
+++ arcade/the_core/11_spring_of_integration/090_pairOfShoes/js/index.js
@@ -1,5 +1,11 @@
-const funcName = () => {
-  return true;
-};
+const pairOfShoes = shoes => {
+    const sizes = shoes.slice().map(shoe => shoe[1]).filter((size,i,arr) => arr.indexOf(size) === i);
+    return sizes.every(size => {
+        const shoesOfThisSize = shoes.slice().filter(shoe => shoe[1] === size);
+        const leftShoes = shoesOfThisSize.filter(shoe => shoe[0] === 0);
+        const rightShoes = shoesOfThisSize.filter(shoe => shoe[0] === 1);
+        return leftShoes.length === rightShoes.length;
+    });
+}
 
-module.exports = { fns: [ funcName ] };
+module.exports = { fns: [ pairOfShoes ] };
