6cd483bc1f9e93efb70a822056593dce4640cb0a
Reid Delahunt <reidpierredelahunt@gmail.com>
2018-07-27 10:22:05 -0700

    js solution for the_core/11/94

--- arcade/the_core/11_spring_of_integration/094_beautifulText/js/index.js
+++ arcade/the_core/11_spring_of_integration/094_beautifulText/js/index.js
@@ -1,5 +1,20 @@
-const funcName = () => {
-  return true;
-};
+const beautifulText = (inputString, l, r) => {
+    const words = inputString.split(' ');
+    for (let width = l; width <= r; width++) {
+        let multiline = [], wordsSlice = words.slice(), broken = false;
+        while (wordsSlice.length > 0) {
+            const newLine = [];
+            while (newLine.join(' ').length < width && wordsSlice.length > 0) {
+                newLine.push(wordsSlice.shift())
+            }
+            if (newLine.join(' ').length > width) { broken = true; break; }
+            else { multiline.push(newLine.join(' ')) }
+        }
+        if (!broken && multiline.every(line => line.split('').length === width)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 module.exports = { fns: [ funcName ] };
--- arcade/the_core/11_spring_of_integration/094_beautifulText/js/test/index.js
+++ arcade/the_core/11_spring_of_integration/094_beautifulText/js/test/index.js
@@ -5,13 +5,97 @@ console.log('fns === ', fns);
 describe("testing ", () => {
   const tests = [
     {
-      fn: '', // name
+      fn: '', // beautifulText(inputString, l, r)
       ins_and_outs: [
         {
           cxt: 'simple test',
-          in: [],
+          in: [
+            "Look at this example of a correct text",
+            5,
+            15
+          ],
           out: true,
-        },
+        },{
+          cxt: 'simple test',
+          in: [
+            "abc def ghi",
+            4,
+            10
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "a a a a a a a a",
+            1,
+            10
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "ab cd fg xyz",
+            1,
+            5
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "aa aa aaaaa aaaaa aaaaa",
+            6,
+            11
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "aa aa aaaaa aaaaa aaaaa",
+            5,
+            10
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "aa aa aaaaa aaaaa aaaaa",
+            6,
+            10
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "aaa aaaaaaa",
+            3,
+            10
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "dht skq dkg",
+            4,
+            10
+          ],
+          out: false,
+        },{
+          cxt: 'simple test',
+          in: [
+            "skspv iakqh ygzwz ntkmi xqhpj",
+            3,
+            7
+          ],
+          out: true,
+        },{
+          cxt: 'simple test',
+          in: [
+            "skspv iakqh ygzwz ntkmi xqhpj",
+            8,
+            9
+          ],
+          out: false,
+        }
       ],
       run: param => {
         const cxt = param.fn.name + '() ' + param.cxt;
